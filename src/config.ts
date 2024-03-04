8ced2dfea65b7c274ff31fa486d06d26edb876dd
geauxweisbeck4 <andrew.weisbeck@gmail.com>
2024-03-04 12:22:24 -0500

    Commit all changes to GitHub and deploy to Netlify, Vercel

--- src/config.ts
+++ src/config.ts
@@ -25,11 +25,11 @@ export const NAV_ITEMS: NavItems = {
 
 export const SITE = {
 	// Your site's detail?
-	name: "Ink",
-	title: "Astro - Ink",
-	description: "Crisp, minimal, personal blog theme for Astro",
-	url: "https://astro-ink.vercel.app",
-	githubUrl: "https://github.com/one-aalam/astro-ink",
+	name: "Rebel's Advocate",
+	title: "Rebel's Advocate",
+	description: "Advocating for those who go against the grain for the greater good of the free world.",
+	url: "https://rebels-advocate.vercel.app",
+	githubUrl: "https://github.com/geauxweisbeck4/rebels-advocate",
 	listDrafts: true,
 	image:
 		"https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png",
@@ -37,14 +37,14 @@ export const SITE = {
 	ytChannelId: "",
 	// Optional, user/author settings (example)
 	// Author: name
-	author: "", // Example: Fred K. Schott
+	author: "Andrew Weisbeck", // Example: Fred K. Schott
 	// Author: Twitter handler
-	authorTwitter: "", // Example: FredKSchott
+	authorTwitter: "GeauxWeisbeck4", // Example: FredKSchott
 	// Author: Image external source
 	authorImage: "", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
 	// Author: Bio
 	authorBio:
-		"Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro",
+		"Andrew is a web developer, entrepreneur, and deep thinker who lives an unconventional, but intentional life. He is currently homeless, but still enjoys every moment of every day with his life partner Lisa.",
 };
 
 // Ink - Theme configuration
---
+++ src/pages/_rebels-advocate.md
@@ -0,0 +1,7 @@
+**Rebel's Advocate** is a blog that explores a wide variety topics that the author finds interesting, from psychology to sports to music to science - you never know what you will find here on any given day. The one thing that you can be assured of is that the author always advocates for those who are different, seek the truth, and want to create positive change in the world. Andrew invites you to read this blog, share it with your friends and family, and share your own opinion to help him understand the world better. 
+
+Thank you for stopping by and we hope you enjoy the **Rebel's Advocate**!
+
+### Advocating for All the Rebels Making the World Better 
+**Maintained By:** Andrew Weisbeck // [@geauxweisbeck4](https://twitter.com/geauxweisbeck4)  // [GeauxWeisbeck4](https://github.com/geauxweisbeck4)
+                
